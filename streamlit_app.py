07f8aae7107929382e95c1114b090a750825ff33
Frank Cegelski <123215405+frankcegelski@users.noreply.github.com>
2023-10-10 16:46:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def get_fruit_load_list():
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
-    return("Thanks for adding " + new_fruit
+    return("Thanks for adding " + new_fruit)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
