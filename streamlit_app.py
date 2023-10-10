eeccd91afa62c4180159180b330ff2220729b0b5
Frank Cegelski <123215405+frankcegelski@users.noreply.github.com>
2023-10-10 13:01:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# normalize the json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display in a dataframe
+streamlit.dataframe(fruityvice_normalized)
 
 
